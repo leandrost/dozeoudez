20d2bf610e2421c4485b18b5c19299ade56ca198
leandrost <leandro.s.thimoteo@gmail.com>
2015-01-03 23:02:04 -0200

    config weinre remote debug and fixinig db sync on test device

--- www/js/services/db.js
+++ www/js/services/db.js
@@ -3,6 +3,6 @@ angular.module("dozeoudez.services")
 .factory("db", function (pouchdb, $window) {
   var db = pouchdb.create("dozeoudez");
   $window.db = db;
-  PouchDB.sync("dozeoudez", "http://localhost:5984/dozeoudez", {live: true, retry: true});
+  PouchDB.sync("dozeoudez", "http://192.168.1.2:5984/dozeoudez", {live: true, retry: true});
   return db;
 });
